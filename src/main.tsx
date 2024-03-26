25b08896de93c7bb1732541eb42166593dfe44f9
Chigozie Oduah <87097037+GhoulKingR@users.noreply.github.com>
2024-03-26 21:32:45 +0100

    some new changes

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import "./index.css";
 import {
-  createBrowserRouter,
+  createHashRouter,
   RouterProvider,
 } from "react-router-dom";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
-    path: "/portfolio-example",
+    path: "/",
     element: <App />
   }
 ])
